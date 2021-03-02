fb620dacc78085483107740a196bb1395f29962f
root <nanmolrao@gmail.com>
2021-03-02 21:34:43 +0530

    script_5b.sh

--- script_5b.sh
+++ script_5b.sh
@@ -2,11 +2,12 @@
 
 echo -e "Current Date: $(date +"%d/%m/%Y")\n"
 currDate=$(date +"%d")
+#currDate=21
 if [ $currDate -le 9 ]
 then
 	currDate=$(echo $currDate | sed 's/'0'/''/')
-	echo "$(cal | sed '3,4 s/'"$currDate"'/'*'/1')"
+	echo "$(cal | sed '3,4 s/\b'"$currDate"'\b/'*'/1')"
 else
-	echo "$(cal | sed '4,6 s/'"$currDate"'/'**'/1')"
+	echo "$(cal | sed '4,6 s/\b'"$currDate"'\b/'**'/1')"
 fi
 
