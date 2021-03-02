f4baf9e2b662af62be1efc638affedc3544763f0
N Anmol <nanmolrao@gmail.com>
2021-03-02 06:09:44 +0530

    script_5a.sh

---
+++ script_5a.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ ! -f "$1"  ||  $# -ne 1 ]]
+then
+	echo "This script only accepts one valid filename as arguement!"
+else
+	statLAT=$(stat "$1" | awk '/Access/ && NR==5 {print $2,$3,$4}')
+	lat=$(date --date="$statLAT" +"%d/%m/%Y  %I:%M %p")
+	echo "FileName: $1"
+	echo "Last Access Time: $lat"
+fi
