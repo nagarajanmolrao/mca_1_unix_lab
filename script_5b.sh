b37eecc5bb830e1cf91d3608a202ab1203916a89
Anmol <nanmolrao@gmail.com>
2021-03-28 21:54:37 +0530

    script_5b.sh

--- script_5b.sh
+++ script_5b.sh
@@ -6,8 +6,8 @@ currDate=$(date +"%d")
 if [ $currDate -le 9 ]
 then
 	currDate=$(echo $currDate | sed 's/'0'/''/')
-	echo "$(cal | sed '3,4 s/\b'"$currDate"'\b/'*'/1')"
+	echo "$(ncal | sed 's/\b'"$currDate"'\b/'*'/1')"
 else
-	echo "$(cal | sed '4,6 s/\b'"$currDate"'\b/'**'/1')"
+	echo "$(ncal | sed 's/\b'"$currDate"'\b/'**'/1')"
 fi
 
