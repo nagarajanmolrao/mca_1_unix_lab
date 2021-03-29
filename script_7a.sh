38acae2ce68d79e16e6d771b90b7c166d7a64832
Anmol <nanmolrao@gmail.com>
2021-03-29 19:18:33 +0530

    script_7a.sh

--- script_7a.sh
+++ script_7a.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 currentTime=$(date +"%H")
-if [[ $currentTime -ge 00 && $currentTime -le 11 ]]
+if [ $currentTime -ge 00 ] && [ $currentTime -le 11 ]
 then
 	echo "Good Morning!"
-elif [[ $currentTime -ge 12 && $currentTime -le 14 ]]
+elif [ $currentTime -ge 12 ] && [ $currentTime -le 14 ]
 then
 	echo "Good Afternoon!"
-elif [[ $currentTime -ge 15 && $currentTime -le 18 ]]
+elif [ $currentTime -ge 15 ] && [ $currentTime -le 18 ]
 then
 	echo "Good Evening!"
 else
